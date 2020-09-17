b2f00765bd0643ae1cb45cd53721c59479fa92d2
Neko-Proger </>
2020-09-17 19:05:53 +0300

    All tasks are done(lesson 1 and lesson 2)! Debug ^_^

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -40,7 +40,7 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean =
  */
 fun daysInMonth(month: Int, year: Int): Int {
     val data = arrayOf(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
-    if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) return data[month - 1] + 1
+    if (month == 2 && ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))) return data[month - 1] + 1
     return data[month - 1]
 }
 
